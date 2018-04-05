1766690d595cf16ee1f23d0c983250c3ef0c55bd
Robert Hardy <robbert@gmail.com>
2018-04-05 23:10:33 +0100

    Setup file for the package.

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name='inference',
+    version='0.1.0',
+    author='Robert Hardy',
+    packages=['inference'],
+    description='FSQ April 2018',
+    install_requires=[
+        "matplotlib",
+        "pandas",
+    ],
+)
